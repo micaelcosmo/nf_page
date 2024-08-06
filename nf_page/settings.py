ae2830a667d5430682852e1f054905b3907b258c
MicaelCosmo <micael.trabalho@hotmail.com>
2024-08-06 14:53:03 -0300

    refactor\core: Folder Loc Ajust

--- nf_page/settings.py
+++ nf_page/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'nf_page.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'main_page', 'templates')
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
