6167e42ffb1c79d2fe660242cfd71ac9b1637a6d
MicaelCosmo <micael.trabalho@hotmail.com>
2024-08-06 21:42:55 -0300

    style

--- portal_nf/navigate/models.py
+++ portal_nf/navigate/models.py
@@ -9,8 +9,8 @@ class User(models.Model):
 
 class Schedules(models.Model):
     name = models.CharField(max_length=250)
-    weekly_day = models.IntegerField()
-    level = models.IntegerField()
+    weekly_day = models.CharField(max_length=100)
+    level = models.CharField(max_length=100)
     start_hour = models.CharField(max_length=100)
     end_hour = models.CharField(max_length=100)
     status = models.CharField(max_length=20)
@@ -19,4 +19,4 @@ class LogAccess(models.Model):
     level = models.CharField(max_length=20)
     description = models.CharField(max_length=500)
     request_type = models.CharField(max_length=10)
-    date = models.DateTimeField()
+    date = models.CharField(max_length=100)
