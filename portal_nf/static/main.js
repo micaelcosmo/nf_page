d56263b537dffba2c5b88c8a652e516ce517d7aa
MicaelCosmo <micael.trabalho@hotmail.com>
2024-09-07 00:30:21 -0300

    refactor: Js Button Refactor

--- portal_nf/static/main.js
+++ portal_nf/static/main.js
@@ -1,8 +1,17 @@
-const experimentalClassButton = document.getElementById('request-experimental-class-button');
-experimentalClassButton.addEventListener('click', () => {
-    window.location.href = 'request_experimental_class/';
+
+document.addEventListener("DOMContentLoaded", function() {
+    const experimentalClassesManagementButton = document.getElementById('experimental-classes-management-button');
+    const experimentalClassButton = document.getElementById('request-experimental-class-button');
+    if (experimentalClassesManagementButton) {
+        experimentalClassesManagementButton.addEventListener('click', () => {
+            window.location.href = 'experimental_classes_management/';
+        });
+    } else if (experimentalClassButton) {
+        experimentalClassButton.addEventListener('click', () => {
+            window.location.href = 'request_experimental_class/';
+        });
+    } else {
+        console.error("Elemento com ID de 'experimentalClass ou experimentalClasses' não encontrado.");
+    }
 });
-const experimentalClassesManagementButton = document.getElementById('experimental-classes-management-button');
-experimentalClassesManagementButton.addEventListener('click', () => {
-    window.location.href = 'experimental_classes_management/';
-});
+    
